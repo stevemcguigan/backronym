250c6abd6a01e6049abbfad3b275a742f7184c0b
stevemcguigan <bitbangerlabs@gmail.com>
2022-07-09 11:41:37 -0400

    fixed newgame

--- public/parser.js
+++ public/parser.js
@@ -146,7 +146,7 @@ ws.onmessage = message => {
            {
              actionsArray.push(new actionItem({
                 label:`play again`,
-                action:`clear_modal_by_id('winner');n("new game started!")start("${gameId}")`
+                action:`clear_modal_by_id('winner');n("new game started!");start("${gameId}")`
               }));
               actionsArray.push(new actionItem({
                 label:`no thanks`,
@@ -320,7 +320,7 @@ ws.onmessage = message => {
             create_new_modal({
                 modal_id:"vote",
                 modal_type:"vote",
-                prompt: `which is the best?`,
+                prompt: `tap on your favorite`,
                 actionsArray: actionsArray,
                 force:true
               });
