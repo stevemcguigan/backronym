c80a2d3f640e6959b31b52c824d5a23aae1ba3b8
Stephen McGuigan <Steve@stephens-imac.mynetworksettings.com>
2022-07-22 11:28:19 -0400

    Changed something

--- public/factories.js
+++ public/factories.js
@@ -17,7 +17,7 @@ function generateLobby()
 		<div class="gameListContainer"><div class="">join a public game</div></div>
 	  <div class="gameListContainer"><div style="margin-top:15px" id="gameList"></div>
 	  </div>
-	  <div class="gameListContainer" style="margin:25px 0px 15;"><div class="">-or-</div></div>
+	  <div class="gameListContainer" style="margin:25px 0px 15px 0px;"><div class="">-or-</div></div>
 	  <div class="lobbyButtons">
 		  <button class="confirmation_button" id = "btnCreate">create new game</button>
 		  <button class="confirmation_button" id = "btnJoinPrivate">join private game</button>
