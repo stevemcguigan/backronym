19d8dac1cd3a481c17c696f4fba2c5d2725a6b16
Stephen McGuigan <Steve@stephens-imac.mynetworksettings.com>
2022-07-22 20:27:08 -0400

    Changed something

--- public/app.js
+++ public/app.js
@@ -43,7 +43,7 @@ function startInstructionsLoop()
         setTimeout(function() {
           id("lobbyInstructionsContainer").innerHTML = generateInstructions();
           startInstructionsLoop();
-        }, 1510000);
+        }, 5000);
       });
     }, 2000);      
   } 
