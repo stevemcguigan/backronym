57ba72627c69a8bc0943af02c5af577fe0f77103
Stephen McGuigan <Steve@stephens-imac.mynetworksettings.com>
2022-07-25 10:39:03 -0400

    Changed something

--- public/factories.js
+++ public/factories.js
@@ -36,7 +36,7 @@ function generateInstructions()
 {
 	return `
 	<div class="gameListContainer" style="margin:20px 0px 20px 0px;font-size:12px;"><div class="">-how to play-</div></div>
-	<div class="gameListContainer" style="margin:20px 0px 20px 0px;"><div style="text-align:center; font-style: italic; font-size:12px; width:75%; max-width:75%">chat with friends while reverse engineering acronyms</div></div>
+	<div class="gameListContainer" style="margin:20px 0px 20px 0px;"><div style="text-align:center; font-style: italic; font-size:12px; width:75%; max-width:75%">chat with friends while reverse engineering acronyms, then vote for your favorite.</div></div>
 
 		<div id="lobbyInstructions" class="instructions" style="transform: scale(.5);    margin-top: -30px;
     	margin-bottom: -30px; font-size:12px;">
