e0720920c2930db14d52130eeb4ac8a2c515643f
Stephen McGuigan <Steve@stephens-imac.mynetworksettings.com>
2022-07-28 11:56:05 -0400

    Changed something

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ wsServer.on("request", request => {
 	});
 	connection.on("close", () => {
 		console.log("connection closed");
+		console.log(this);
 	});
 	connection.on("message", message => {
 	// could fail if client sends bad JSON
