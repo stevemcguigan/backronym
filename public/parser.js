7d57a9020ce188471f2d01441d69d02dc7a899d9
Stephen McGuigan <Steve@stephens-imac.mynetworksettings.com>
2022-08-31 11:47:30 -0400

    committing before working on punctuation stuff

--- public/parser.js
+++ public/parser.js
@@ -1,6 +1,6 @@
 // incoming responses from the server are parsed here
 
-let ws = new WebSocket("ws://44.204.232.121:9090")
+let ws = new WebSocket("ws://52.23.241.90:9090")
 //let ws = new WebSocket("ws://192.168.99.41:9090")
 // 
 // 192.168.99.41
