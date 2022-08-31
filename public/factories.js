e8851630b2214b18ddfc2e4fad473b420328ddb8
Stephen McGuigan <Steve@stephens-imac.mynetworksettings.com>
2022-08-30 22:38:23 -0400

    changed stuff

--- public/factories.js
+++ public/factories.js
@@ -29,7 +29,7 @@ function generateLobby()
 			${generateInstructions()}
 		</div>
 	  <!--<button id = "btnJoin">Join Game</button>-->
-	  <input type = "text" id = "txtGameId">`;
+	  <input type = "text" id = "txtGameId">`; 
 	return markup;
 }
 
