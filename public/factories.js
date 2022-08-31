709aa958cb6728192866d4dcd84904203c151b5e
Stephen McGuigan <Steve@stephens-imac.mynetworksettings.com>
2022-08-30 22:40:50 -0400

    changed stuff

--- public/factories.js
+++ public/factories.js
@@ -379,7 +379,7 @@ inputs.forEach(input => {
 function generateGame()
 {
 	let markup = `
-	<div id="titleBar" class=""><span id="logoBar">backronym</span><span id="menu" onclick="openMenu()">☰</span></div>
+	<div id="titleBar" class=""><span id="logoBar">backronym</span><span id="menu" onclick="openMenu()">&lt;&lt;</span><span id="menu" onclick="openMenu()">☰</span></div>
 	<div class="acronymContainer">
 		<button id = "${host ? 'btnStart' : 'btnWait'}" class="animate__animated animate__zoomIn">${host ? 'start' : 'waiting...'}</button>			  	
 	</div>
