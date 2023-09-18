706866d8e039435fb0a80992d3be7dedc2f61290
Stephen McGuigan <Steve@stephens-imac.mynetworksettings.com>
2023-09-18 11:38:11 -0400

    oops x2

--- public/parser.js
+++ public/parser.js
@@ -1,6 +1,6 @@
 // incoming responses from the server are parsed here
 
-let ws = new WebSocket("ws://localhost:9090")
+let ws = new WebSocket("ws://poop.ovh:9090")
 //let ws = new WebSocket("ws://192.168.99.41:9090")
 // 
 // 192.168.99.41
